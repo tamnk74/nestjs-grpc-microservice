tomnk/nestjs_grpc_service

feat: sample service using grpc for microservices

src/exceptions/index.ts
@@ -1,0 +1,3 @@
+export * from './all-exception.filter';
+export * from './api.exception';
+export * from './common.exception';

tamnk74/nestjs-grpc-microservice

src/exceptions/index.ts
@@ -1,0 +1,3 @@
+export * from './grpc-exception.filter';
+export * from './api.exception';
+export * from './common.exception';